f10958ebbafa79a5d70cf0abc46a9f5ddf24dd20
pavan <pavanrm86@gmail.com>
2020-11-14 21:27:53 +0530

    firstcommit

---
+++ src/test/java/mypackage/myclass1.java
@@ -0,0 +1,10 @@
+package mypackage;
+
+import org.junit.jupiter.api.Test;
+
+public class myclass1 {
+@Test
+public void method1() {
+	System.out.print("inside method1");
+}
+}
---
+++ src/test/java/mypackage/myclass2.java
@@ -0,0 +1,10 @@
+package mypackage;
+
+import org.junit.jupiter.api.Test;
+
+public class myclass2 {
+@Test
+public void method2() {
+	System.out.print("inside method2");
+}
+}
